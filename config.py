ddaf0682fe5dee3ef57508fcbf6822116009f0eb
22f3002774-droid <22f3002774@ds.study.iitm.ac.in>
2026-07-14 17:00:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-
+EMAIL = "22f3002774@ds.study.iitm.ac.in"   
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjMwMDI3NzRAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDAyODYwNCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NDYzMzQwNH0.S1u-ZcXbFYzJsv5xnv2QGnk0cdxxEPJNtBK6x74XeJ4"
+AIPIPE_BASE = "https://aipipe.org/openai/v1"
+TEXT_MODEL = "gpt-4o"
